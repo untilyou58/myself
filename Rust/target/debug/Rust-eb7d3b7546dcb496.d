e2c0b4bd5d448708493eaa4b049d2bf8d681f703
Dang Dinh Sang <sangxoay58@gmail.com>
2020-03-18 14:26:52 +0700

    [add] git ignore

--- Rust/target/debug/Rust-eb7d3b7546dcb496.d
+++
@@ -1,1 +0,0 @@
-/home/hika/Documents/MySelf/myself/Rust/target/debug/Rust-eb7d3b7546dcb496: /home/hika/Documents/MySelf/myself/Rust/src/main.rs
--- Rust/target/debug/deps/Rust-eb7d3b7546dcb496.d
+++
@@ -1,5 +0,0 @@
-/home/hika/Documents/MySelf/myself/Rust/target/debug/deps/Rust-eb7d3b7546dcb496: src/main.rs
-
-/home/hika/Documents/MySelf/myself/Rust/target/debug/deps/Rust-eb7d3b7546dcb496.d: src/main.rs
-
-src/main.rs:
