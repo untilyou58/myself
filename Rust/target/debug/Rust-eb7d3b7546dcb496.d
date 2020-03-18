0497da020b907b647d1d2436ece6617aa1ecb4e2
Dang Dinh Sang <sangxoay58@gmail.com>
2020-03-18 10:56:30 +0700

    [add] whale css

---
+++ Rust/target/debug/Rust-eb7d3b7546dcb496.d
@@ -0,0 +1,1 @@
+/home/hika/Documents/MySelf/myself/Rust/target/debug/Rust-eb7d3b7546dcb496: /home/hika/Documents/MySelf/myself/Rust/src/main.rs
---
+++ Rust/target/debug/deps/Rust-eb7d3b7546dcb496.d
@@ -0,0 +1,5 @@
+/home/hika/Documents/MySelf/myself/Rust/target/debug/deps/Rust-eb7d3b7546dcb496: src/main.rs
+
+/home/hika/Documents/MySelf/myself/Rust/target/debug/deps/Rust-eb7d3b7546dcb496.d: src/main.rs
+
+src/main.rs:
